c26bf1f8b099af5f5b7b61f814ff4d395f3bb29d
Maksym Vlasenko <191160@nuos.edu.ua>
2020-12-24 20:44:24 +0300

    fixed issues after review

--- src/com/maksim/Main.java
+++ src/com/maksim/Main.java
@@ -2,47 +2,63 @@ package com.maksim;
 
 import com.maksim.model.Phone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        Phone[] subscribers = createSubscribers();
-        showSubscribersWithInCityCallsDurationGreaterThan(subscribers, 20);
-        showSubscribersWhoUsedInterCityCalls(subscribers);
-        showSubscribersWhomAccountNumberIsBetween(subscribers, 1000,5000);
+        Main main = new Main();
+        Phone[] subscribers = main.createSubscribers();
+        Phone[] subscribers1 = main.getSubscribersWithInCityCallsDurationGreaterThan(subscribers, 20);
+        System.out.printf("Subscribers with in city calls duration greater than %d minutes:%n", 20);
+        main.printSubscribers(subscribers1);
+        Phone[] subscribers2 = main.getSubscribersWhoUsedInterCityCalls(subscribers);
+        System.out.println("Subscribers who used inter city calls: ");
+        main.printSubscribers(subscribers2);
+        Phone[] subscribers3 = main.getSubscribersWhomAccountNumberIsBetween(subscribers, 1000, 5000);
+        System.out.printf("Subscribers whom account number is between %d and %d:%n", 1000, 5000);
+        main.printSubscribers(subscribers3);
     }
 
-    private static void showSubscribersWhomAccountNumberIsBetween(Phone[] subscribers, int start, int end) {
-        System.out.printf("Subscribers whom account number is between %d and %d:%n", start, end);
+    private void printSubscribers(Phone[] subscribers) {
+        for (Phone phone : subscribers) {
+            System.out.println(phone);
+        }
+    }
+
+    private Phone[] getSubscribersWhomAccountNumberIsBetween(Phone[] subscribers, int start, int end) {
+        List<Phone> results = new ArrayList<>();
         for (Phone subscriber : subscribers) {
             int accountNumber = subscriber.getAccountNumber();
             if (accountNumber >= start && accountNumber <= end) {
-                System.out.println(subscriber.toString());
+                results.add(subscriber);
             }
         }
-        System.out.println();
+        return results.toArray(new Phone[0]);
     }
 
-    private static void showSubscribersWhoUsedInterCityCalls(Phone[] subscribers) {
-        System.out.println("Subscribers who used inter city calls: ");
+    private Phone[] getSubscribersWhoUsedInterCityCalls(Phone[] subscribers) {
+        List<Phone> results = new ArrayList<>();
         for (Phone subscriber : subscribers) {
             if (subscriber.getInterCityCallsMinutes() > 0) {
-                System.out.println(subscriber.toString());
+                results.add(subscriber);
             }
         }
-        System.out.println();
+        return results.toArray(new Phone[0]);
     }
 
-    private static void showSubscribersWithInCityCallsDurationGreaterThan(Phone[] subscribers, int duration) {
-        System.out.printf("Subscribers with in city calls duration greater than %d minutes:%n", duration);
+    private Phone[] getSubscribersWithInCityCallsDurationGreaterThan(Phone[] subscribers, int duration) {
+        List<Phone> results = new ArrayList<>();
         for (Phone subscriber : subscribers) {
             if (subscriber.getInCityCallsMinutes() > duration) {
-                System.out.println(subscriber.toString());
+                results.add(subscriber);
             }
         }
-        System.out.println();
+        return results.toArray(new Phone[0]);
     }
 
-    private static Phone[] createSubscribers() {
+    private Phone[] createSubscribers() {
         return new Phone[]{
                 new Phone(1, "Alex", "Petrov", "Ivanovich", 1543, 15, 0),
                 new Phone(2, "Oleg", "Ivanov", "Olegovich", 1209, 56, 20),
